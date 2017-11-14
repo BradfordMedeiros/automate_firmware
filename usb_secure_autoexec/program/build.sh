637000b92cd3886a982cdc74f0dd4f86b1177a3b
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-11-13 23:28:45 -0800

    add build scripts w/ sub

--- usb_secure_autoexec/program/build.sh
+++ usb_secure_autoexec/program/build.sh
@@ -1,4 +1,25 @@
+
+#!/bin/bash
+
+AUTOMATE_KEY=$1
+USB_FILE_DIRECTORY=$2
+
+if [ $# != 2 ]
+  then
+    echo "Invalid number of arguments to build usb" >&2
+    echo "Usage: build.sh <AUTOMATE_KEY> <USB_FILE_DIRECTORY>" >&2
+    exit 1
+  else 
+    echo "Using: AUTOMATE_KEY: $AUTOMATE_KEY; USB_FILE_DIRECTORY: $USB_FILE_DIRECTORY"
+fi
+
 mkdir ./build
+
 cargo build --release
+
 cp target/release/usb_secure_autoexec ./build
 cp ./run_scripts.sh ./build
+
+sed -i "s/<SCRIPT_PATH>/$USB_FILE_DIRECTORY/g" ./build/run_scripts.sh
+sed -i "s/<SYS_PATH>/$AUTOMATE_KEY/g" ./build/run_scripts.sh
+
---
+++ usb_secure_autoexec/program/run_scripts.sh
@@ -0,0 +1,2 @@
+
+./usb_secure_autoexec -s <SCRIPT_PATH> -k <SCRIPT_PATH>/automate.key -m <SYS_PATH>
