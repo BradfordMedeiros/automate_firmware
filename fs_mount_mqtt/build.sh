5d9aa89326c5c61c8355b1f8e4bf4178b6d1ed80
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-11-24 12:32:19 -0800

    seperate directories for client and daemon in build

--- fs_mount_mqtt/build.sh
+++ fs_mount_mqtt/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 mkdir ./build
+mkdir -p ./build/client
+mkdir -p ./build/daemon
 
 (cd ./daemon/ && source ./install.sh && ./build.sh)
-cp ./daemon/build/* ./build/
+cp ./daemon/build/* ./build/daemon
 
 (cd ./client/ && source ./install.sh && ./build.sh)
-cp ./client/build/* ./build
+cp ./client/build/* ./build/client
