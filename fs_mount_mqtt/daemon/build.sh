af0a4231df6c13f0c2ac86b787ca637570100ac5
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-11-24 00:12:41 -0800

    -

--- fs_mount_mqtt/daemon/build.sh
+++ fs_mount_mqtt/daemon/build.sh
@@ -4,4 +4,4 @@ mkdir -p ./build
 go build -o ./build/fs_mount_mqttd -ldflags "-s -w" ./src/*
 cp ./install_res/install.sh ./build/install.sh
 cp ./install_res/uninstall.sh ./build/uninstall.sh
-cp ./install_res/fs_mount_mqtt ./build//fs_mount_mqtt
+cp ./install_res/fs_mount_mqtt ./build/fs_mount_mqtt
--- fs_mount_mqtt/daemon/build/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo service fs_mount_mqtt stop
-sudo rm -rf /opt/fs_mount_mqtt
-sudo rm -rf /etc/init.d/fs_mount_mqtt
-sudo systemctl daemon-reload
