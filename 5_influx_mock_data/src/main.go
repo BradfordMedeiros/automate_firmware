0ff53e809662b014240719979c34d18ef0ff017f
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-12-18 00:04:45 -0800

    basic settings working without influx

--- 5_influx_mock_data/src/main.go
+++ 5_influx_mock_data/src/main.go
@@ -7,6 +7,7 @@ if categorical: specify list, and then uniformly populate the data
 if number, specify the range, and uniformly populate the data
 
 specify time range
+--interval m --ic 4
  */
 
 package main
@@ -15,6 +16,8 @@ import (
 	"flag"
 	"fmt"
 	"time"
+	"math/rand"
+	"strconv"
 )
 
 type param struct {
@@ -38,8 +41,8 @@ func print_error() {
 
 type settings struct {
 	topic string
-	min_value float64
-	max_value float64
+	min_value int
+	max_value int
 	data_type string
 
 	fromYear int
@@ -50,11 +53,83 @@ type settings struct {
 
 	fromDay int
 	untilDay int
+
+	interval string
 }
 
-func run_program_with_settings(sett *settings ){
-  fmt.Println("settings topic is: ", sett)
+func log_data_to_influx(time_to_log_data time.Time, topic string, value string){
+	fmt.Println("@: ", time_to_log_data)
+	fmt.Println("log: ", topic, " with value: ", value)
 }
+
+func get_interval_duration(interval string) time.Duration {
+	duration := time.Second;
+	if interval == "s" {
+		duration = time.Second
+		fmt.Println("second interval")
+	}else if interval == "m" {
+		duration = time.Minute
+		fmt.Println("minute interval")
+	}else if interval == "h" {
+		duration = time.Hour
+		fmt.Println("hour interval")
+	}else if interval == "d" {
+		duration = time.Hour * 24
+		fmt.Println("day interval")
+	}else {
+		fmt.Println("invalid interval")
+	}
+	return duration
+}
+
+func get_random_value (min_value int, max_value int) int {
+	return (rand.Intn((max_value - min_value)) + min_value)
+}
+
+func run_program_with_settings(settings *settings ) {
+	fmt.Println("settings topic is: ", settings)
+
+	fromTime := time.Date(
+		settings.fromYear, // year
+		time.Month(12), // month
+		settings.fromDay, // day
+		12, // hour
+		0, // min
+		0, // s
+		0, // ns
+		time.UTC,
+	)
+	untilTime := time.Date(
+		settings.untilYear, // year
+		time.Month(12), // month
+		settings.untilDay, // day
+		12, // hour
+		0, // min
+		0, // s
+		0, // ns
+		time.UTC,
+	)
+
+	fmt.Println("begin time: ", fromTime)
+	fmt.Println("end time: ", untilTime)
+
+	duration := get_interval_duration(settings.interval)
+
+	currentTime := fromTime
+
+	count := 0
+
+	for currentTime.Before(untilTime){
+		currentTime = currentTime.Add(duration)
+		log_data_to_influx(currentTime, "test topic", strconv.Itoa(get_random_value(settings.min_value, settings.max_value)))
+
+		count ++
+	}
+
+	fmt.Println("total logged data: ", count)
+
+}
+
 
 func main() {
 	topicFlag := param{is_set: false}
@@ -63,8 +138,11 @@ func main() {
 	typeFlag := param{is_set: false}
 	flag.Var(&typeFlag, "type", "<categorical/numeric>")
 
-	minValue := flag.Float64("min", 1, "minimum value to write for numeric data")
-	maxValue := flag.Float64("max", 100, "maxmum value to write for numeric data")
+	intervalFlag := param{is_set: false}
+	flag.Var(&intervalFlag, "interval", "<month, day, minute, second>")
+
+	minValue := flag.Int("min", 1, "minimum value to write for numeric data")
+	maxValue := flag.Int("max", 100, "maxmum value to write for numeric data")
 
 	today := time.Now()
 	fromYear := flag.Int("fromYear", today.Year(), "start year")
@@ -87,31 +165,23 @@ func main() {
 		untilMonth: *untilMonth,
 		fromDay: *fromDay,
 		untilDay: *untilDay,
+		interval: intervalFlag.value,
 	}
 
-	fromTime := time.Date(
-		program_settings.fromYear,  // year
-		time.Month(12),		    // month
-		program_settings.fromDay,   // day
-		0,			    // hour
-		0,			    // min
-		0,			    // s
-		0,			    // ns
-		time.UTC,
-	)
-	untilTime := time.Date(
-		program_settings.fromYear,  // year
-		time.Month(12),		    // month
-		program_settings.fromDay,   // day
-		12,			    // hour
-		0,			    // min
-		0,			    // s
-		0,			    // ns
-		time.UTC,
-	)
+	run_program_with_settings(&program_settings)
 
-	fmt.Println("begin time: ", fromTime)
-	fmt.Println("end time: ", untilTime)
+
+
+	/*newDate := fromTime.AddDate(0,0,1)
+
+	oneSecond := time.Duration(time.Second)
+	oneMinute := time.Duration(time.Minute)
+
+
+	fmt.Println("new time: ", newDate)
+	fmt.Println("duration: ", oneSecond)
+	fmt.Println("minute: ", oneMinute)
+	fmt.Println("time diff is: ", newDate.After(fromTime))*/
 
 	/*if topicFlag.is_set {
 		fmt.Println("topic is: ", topicFlag.value)
