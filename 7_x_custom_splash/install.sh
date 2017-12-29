b1b8831723dd84986a9af92e27f9d7d93e563856
brad <bradmedeiros0@gmail.com>
2017-12-29 04:01:53 +0000

    update splash screen files

--- 7_x_custom_splash/install.sh
+++ 7_x_custom_splash/install.sh
@@ -1,2 +1,4 @@
-
 sudo apt-get install fbi
+sudo cp ./splash.png /opt/splash.png
+sudo cp splashscreen.service /etc/systemd/system
+sudo systemctl enable splashscreen
---
+++ 7_x_custom_splash/show_splash.sh
@@ -0,0 +1,1 @@
+/usr/bin/fbi -t 3 -1  --noverbose -a /opt/splash.png
