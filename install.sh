122dc273ca33b7181bc5769d2ea700648d85d486
brad <bradmedeiros0@gmail.com>
2018-04-04 01:55:12 -0700

    install docker in install script

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+# This file is responsible for invoking build all the necessary projects and then copying them into the correct places
+# This may eventually become a proper make file at some point
+
+# Basically automate is a raspberry pi, put inside a docker container, with an SSH service exposed outside of the docker container so we
+# can upgrade the pi.  Volumes mapped to store persistant data + allow gpio access.
+
+# Install docker onto this machine
+curl -sSL https://get.docker.com | sh
