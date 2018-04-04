d5bdc64fe068978bdf57f42fb64232e6b0543a34
brad <bradmedeiros0@gmail.com>
2018-04-04 03:14:53 -0700

    create automate user, change default pws

--- install.sh
+++ install.sh
@@ -6,5 +6,21 @@
 # Basically automate is a raspberry pi, put inside a docker container, with an SSH service exposed outside of the docker container so we
 # can upgrade the pi.  Volumes mapped to store persistant data + allow gpio access.
 
-# Install docker onto this machine
-curl -sSL https://get.docker.com | sh
+
+### Install dependencies section ###
+
+# Install docker onto this machine.  Is there a better way of doing this?
+if [ -z "$(which docker)" ]; then
+    echo "docker is not yet installed"
+    #curl -sSL https://get.docker.com | sh
+fi 
+
+### Configuring software into right places section ###
+
+sudo service ssh start
+echo "pi:password" | sudo chpasswd  # hardcoding password to password (as opposed to pi's default password of raspberry).  This probably needs to be done manually per device before shipping and saved. 
+
+sudo useradd automate
+echo "automate:password" | sudo chpasswd
+
+
